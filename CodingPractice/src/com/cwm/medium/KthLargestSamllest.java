5aae1748d555e2d1b7bec1ac32aa89399f0073ff
MANISH <MANISH@DESKTOP-UHTP9CS>
2022-05-30 17:28:28 +0530

    added Kth smallest optimal solution.

--- CodingPractice/src/com/cwm/medium/KthLargestSamllest.java
+++ CodingPractice/src/com/cwm/medium/KthLargestSamllest.java
@@ -9,11 +9,26 @@
  */
 package com.cwm.medium;
 
+import java.util.Arrays;
+
 public class KthLargestSamllest {
 
 	public static void main(String[] args) {
 		//Initializing input
+		int[] arr = {6,9,3,2,19,14};
+		int k=4;
+		System.out.println(k+"th smallest element in given array is: "+findKthSmallest(arr, k));
 		
+	}
+	//this method is used to find kth smallest element in given unsorted array
+	//time complexity for this solution is O(N log N) and auxilary space complexity is O(1)
+	public static int findKthSmallest(int[] arr, int k) {
+		int result =0;
+		//sort the array
+		Arrays.sort(arr); //O(N log N)
+		//returning k-1th index value from the array.
+		result = arr[k-1];
+		return result;
 	}
 
 }
