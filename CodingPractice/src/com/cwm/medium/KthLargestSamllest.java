aa1d65eee7d5fe4d64ca7b9f56bcb2149ac4c75d
MANISH <MANISH@DESKTOP-UHTP9CS>
2022-05-30 12:58:56 +0530

    adding repo structure

---
+++ CodingPractice/src/com/cwm/medium/KthLargestSamllest.java
@@ -0,0 +1,19 @@
+/*Kth Smallest/Largest Element In Unsorted Array Difficulty Level - Medium
+ *Problem Statement
+	Given an array and a number k where k is smaller than the size of the array, 
+	we need to find the Kth  smallest element in the given array.
+	It is given that all elements in the array are distinct. for eg.
+	Input: arr[] = {6, 9, 3, 2, 19, 14}
+			k = 3
+	Output: 6
+ */
+package com.cwm.medium;
+
+public class KthLargestSamllest {
+
+	public static void main(String[] args) {
+		//Initializing input
+		
+	}
+
+}
